095e296e119ba02c30a51f7d13371813800b5b41
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-06-06 12:28:01 +0200

    front page styling and fitting to size

--- wp-content/themes/html5blank-stable/index.php
+++ wp-content/themes/html5blank-stable/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-	<div class="container" style="padding-top: 250px; padding-bottom: 120px;">
+	<div class="container" style="padding-top: 200px; padding-bottom: 120px;">
 		<h1 class="text-center title">Your Health is the World</h1>
 			<div class="separator"></div>
 				<p style="padding-bottom: 50px; font-size: 30px;" class="lead text-center">Our mission is providing high quality and nutritious<br> 
@@ -8,13 +8,69 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-7">
-				<h3>Discover Our Trademarks</h3>
-				<img style="height: 320px;" src="/wp-content/themes/html5blank-stable/img/front_/discover.png">
+
+
+			<div class="col-sm-7 trademarks">
+			<h3 style="width: 650px; right: 0; position: absolute; margin: auto;">Discover Our Trademarks</h3>
+				<div class="row">
+					<div class="col-sm-6">
+						<div>
+							<p style="padding-top: 70px;">Tropicana a range of fresh and long-life juices in<br>
+								different flavors.</p>
+							<p><a>discover more</a></p>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<img style="margin-top: 70px; position: relative;" src="/wp-content/themes/html5blank-stable/img/discoverus_image.png">
+					</div>
+				</div>
+				<div class="divider"></div>
+					<div class='row'>
+						<div class='col-md-6 slider_fp'>
+						  <div class="carousel slide media-carousel" id="recommendations">
+						    <div class="carousel-inner">
+						      <div class="item  active">
+						        <div class="row">
+						          <div class="col-md-3">
+						            <a href="#"><img alt="" data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>          
+						          <div class="col-md-3">
+						            <a href="#"><img alt=""  data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>
+						          <div class="col-md-3">
+						            <a href="#"><img alt=""  data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>
+						          <div class="col-md-3">
+						            <a href="#"><img alt="" data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>            
+						        </div>
+						      </div>
+						      <div class="item">
+						        <div class="row">
+						          <div class="col-md-3">
+						            <a href="#"><img alt="" data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>          
+						          <div class="col-md-3">
+						            <a href="#"><img alt=""  data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>
+						          <div class="col-md-3">
+						            <a href="#"><img alt=""  data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>
+						          <div class="col-md-3">
+						            <a href="#"><img alt="" data-src="/wp-content/themes/html5blank-stable/img/front_slider.png"></a>
+						          </div>            
+						        </div>
+						      </div>
+						    </div>
+						    <a data-slide="prev" href="#recommendations" class="left"><span class="glyphicon glyphicon-chevron-left leftSlider"></span></a>
+						    <a data-slide="next" href="#recommendations" class="right"><span class="glyphicon glyphicon-chevron-right rightSlider"></span></a>
+						  </div>                          
+						</div>
+					</div>
+
 			</div>
-			<div class="col-sm-5">
-				<h3>Visit Us</h3>
-				<img style="height: 320px;" src="/wp-content/themes/html5blank-stable/img/front_/visitus.png">
+			<div class="col-sm-5 visitus">
+				<h3 style="width: 320px; right: 0; position: absolute; margin: auto;">Visit Us</h3>
 			</div>
 		</div>
 		<div class="separator"></div>
@@ -39,5 +95,14 @@
 			</div>
 		</div>
 	</div>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+		  $('#recommendations').carousel({
+		    pause: true,
+		    interval: false,
+		  });
+		});
+	</script>
 
 <?php get_footer(); ?>
