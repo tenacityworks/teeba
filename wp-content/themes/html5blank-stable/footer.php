cd9e950df352018507160b58161735a345278a02
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-06-13 13:29:23 +0200

    footer text edit

--- wp-content/themes/html5blank-stable/footer.php
+++ wp-content/themes/html5blank-stable/footer.php
@@ -7,7 +7,7 @@
 					<div class="row">
 						<div class="col-md-4">
 							<div>
-								<h4 style="color: #fff;">Contact Us</h4>
+								<h4 style="color: #fff; font-size: 26px;">Contact Us</h4>
 								<p class="pfooter">Amman - Um Al Basatein - Airport Road<br> 
 										P.O. Box 35076 Amman 11180 Jordan<br>
 										Tel.: +962 6 4290868<br>
