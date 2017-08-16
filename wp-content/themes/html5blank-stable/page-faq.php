9404b3ecd3a46bf4ce049d7e3e2ea5b636464390
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-16 15:33:06 +0200

    faq page

---
+++ wp-content/themes/html5blank-stable/page-faq.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="spacetop"></div>
+		<div class="container">
+			<h2 id="about">FAQ</h2>		
+			<div class="col-sm-12">
+				<div class="faq">
+					<h3>Q. Auisque nulla eros, rutrum id interdum id, elementum sit amet massa.</h3>
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."</p>
+				</div>
+				<div class="faq">
+					<h3>Q. Auisque nulla eros, rutrum id interdum id, elementum sit amet massa.</h3>
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."</p>
+				</div>
+				<div class="faq">
+					<h3>Q. Auisque nulla eros, rutrum id interdum id, elementum sit amet massa.</h3>
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."</p>
+				</div>
+				<div class="faq">
+					<h3>Q. Auisque nulla eros, rutrum id interdum id, elementum sit amet massa.</h3>
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."</p>
+				</div>
+				<div class="faq">
+					<h3>Q. Auisque nulla eros, rutrum id interdum id, elementum sit amet massa.</h3>
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."</p>
+				</div>
+				<div class="faq">
+					<h3>Q. Auisque nulla eros, rutrum id interdum id, elementum sit amet massa.</h3>
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."</p>
+				</div>
+			</div>
+
+		</div>
+		
+		<!-- section end -->	
+
+<?php get_footer(); ?>
