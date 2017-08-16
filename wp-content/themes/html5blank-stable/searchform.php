7317e828f3b0d2332a719ae189b056f273f76c3c
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-16 12:21:46 +0200

    search form

--- wp-content/themes/html5blank-stable/searchform.php
+++ wp-content/themes/html5blank-stable/searchform.php
@@ -1,6 +1,10 @@
 <!-- search -->
-<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
-	<input class="search-input" type="search" name="s" placeholder="<?php _e( 'To search, type and hit enter.', 'html5blank' ); ?>">
-	<button class="search-submit" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
+<form id="search" method="get" action="<?php echo home_url(); ?>" role="search">
+	<div id="label"><label for="search-terms" id="search-label">search</label></div>
+	<div id="input"><input type="search" name="s" id="search-terms" placeholder=" "></div>
 </form>
+<!-- <form class="search" method="get" action="<?php //echo home_url(); ?>" role="search">
+	<input class="search-input" type="search" name="s" placeholder="<?php //_e( 'To adASsearch, type and hit enter.', 'html5blank' ); ?>">
+	<button class="search-submit" type="submit" role="button"><?php //_e( 'Search', 'html5blank' ); ?></button>
+</form> -->
 <!-- /search -->
