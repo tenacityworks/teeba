236f0a31897a687c3eebb759052d02a6bac8f1d9
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-16 15:00:24 +0200

    contact page and styling

---
+++ wp-content/themes/html5blank-stable/page-contact.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="spacetop"></div>
+		<div class="container">
+			<h2 id="about">Contact</h2>		
+			<div class="col-sm-5 ">
+				<div class="our-vision">
+				<h3>Our Vision</h3>
+					<p>P.O. Box 35076<br>
+						Amman 11180<br>
+						Jordan<br> 
+						Tel.: +962 6 4290868<br>
+						Fax: +962 6 4290862 038
+					</p>
+				</div>	
+			</div>
+			<div class="col-sm-7">
+				<div class="contact-map">
+					<h3>Visit Us</h3>
+				</div>
+			</div>
+
+		</div>
+		
+		<!-- section end -->	
+
+<?php get_footer(); ?>
