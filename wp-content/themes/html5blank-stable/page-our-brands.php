cdcefd6e67c405087dc588e70347feb0336e76dc
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-17 15:32:40 +0200

    brands page

---
+++ wp-content/themes/html5blank-stable/page-our-brands.php
@@ -0,0 +1,107 @@
+<?php get_header(); ?>
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="spacetop-pages"></div>
+		<div class="container">
+			<h2 id="about">Our Brands</h2>		
+			<div  class="col-sm-12">
+				<div class="brands-tabs">
+				<h3>7DAYS</h3>
+					<div class="col-sm-9 brands-tabs-text">
+						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
+
+					</div>
+					<div class="col-sm-3 brands-tabs-image">
+						<div class="brands">
+							<img style="width: 100%; height: auto;" src="<?php echo get_template_directory_uri(); ?>/img/7days.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-12">
+				<div class="brands-tabs">
+				<h3>Tropicana</h3>
+					<div class="col-sm-9 brands-tabs-text">
+						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
+
+					</div>
+					<div class="col-sm-3 brands-tabs-image">
+						<div class="brands">
+							<img style="width: 100%; height: auto;" src="<?php echo get_template_directory_uri(); ?>/img/7days.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		<div class="col-sm-12">
+			<div class="brands-tabs">
+				<h3>Teeba</h3>
+					<div class="col-sm-9 brands-tabs-text">
+						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
+
+					</div>
+					<div class="col-sm-3 brands-tabs-image">
+						<div class="brands">
+							<img style="width: 100%; height: auto;" src="<?php echo get_template_directory_uri(); ?>/img/7days.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+		<div class="col-sm-12">
+			<div class="brands-tabs">
+				<h3>Almarai</h3>
+					<div class="col-sm-9 brands-tabs-text">
+						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
+
+					</div>
+					<div class="col-sm-3 brands-tabs-image">
+						<div class="brands">
+							<img style="width: 100%; height: auto;" src="<?php echo get_template_directory_uri(); ?>/img/7days.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<div class="col-sm-12">
+			<div class="brands-tabs">
+				<h3>Pepsico</h3>
+					<div class="col-sm-9 brands-tabs-text">
+						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
+
+					</div>
+					<div class="col-sm-3 brands-tabs-image">
+						<div class="brands">
+							<img style="width: 100%; height: auto;" src="<?php echo get_template_directory_uri(); ?>/img/7days.png">
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+		
+		<!-- section end -->	
+
+<?php get_footer(); ?>
