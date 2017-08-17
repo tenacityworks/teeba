3f0033155793faa368753c42063eb59c76127a90
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-17 16:27:35 +0200

    about teeba page

---
+++ wp-content/themes/html5blank-stable/page-about-teeba.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="spacetop-pages"></div>
+		<div class="container">
+			<h2 id="about">About Teeba</h2>		
+			<div class="col-sm-8">
+				<div class="about-teeba">
+				<h3>Our Vision</h3>
+					<p style="padding: 10px;">To be the consumers’ preferred product of choice.</p><br>
+					<h3>Our Mission</h3>
+					<p style="padding: 10px;">
+						To provide high quality and nutritious foods and beverages to consumers.
+					</p>
+					<h3>Our Values</h3>
+					<p style="padding: 10px;">
+						Excellence: We aim to always excel by delivering the best high quality products.<br>
+						Passion: We encourage our employees and aim for outstanding results by instilling in them the love of achievement.<br>
+						Honesty: We conduct all our business transactions with the utmost transparency.<br>
+						Experience: We aim to stay up to date and enhance our abilities and experience.
+					</p>
+					<h3>Teeba Investment for Developed Food Processing Company</h3>
+					<p style="padding: 10px;">
+						Established in 2004, Teeba Investment for Developed Food Processing Company is a fully owned subsidiary of the International Dairy and Juice Company, which was established in 2009 as a joint venture between Almarai and PepsiCo.  It has a number of competitive advantages, most notably the integration of Almarai expertise in the dairy industry in the region and the prominent international presence of PepsiCo International.<br> 
+
+						The company’s head offices are located in Um Al-Basateen in Amman and its operation unit is situated in Al-Hallabat area in Zarqa. The company owns Al-Atheer and Al Tabee’a farms which provide the fresh milk used in producing dairy products. The company’s warehouses are strategically distributed around Jordan and located in Amman, Al-Hallabat, Irbid, Al-Karak, Ma’an and Aqaba.<br>
+
+						Teeba products include a comprehensive range of dairy products,Almarai products, a variety of Tropicana juice flavors and 7Days products baked from fresh natural ingredients.
+					</p>
+				</div>	
+			</div>
+			<div class="col-sm-4">
+				<div class="join-us">
+					<h3>Visit Us</h3>
+				</div>
+			</div>
+
+		</div>
+		
+		<!-- section end -->	
+
+<?php get_footer(); ?>
