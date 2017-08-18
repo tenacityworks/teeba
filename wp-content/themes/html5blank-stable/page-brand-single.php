fb0327018dc506faa8229fc3623fab2bdc69726c
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-18 11:20:09 +0200

    branch pages

---
+++ wp-content/themes/html5blank-stable/page-brand-single.php
@@ -0,0 +1,121 @@
+<?php /* Template Name: Template - Brand Single */ ?>
+
+<?php get_header(); ?>
+		<!-- section start -->
+		<!-- ================ -->
+	<div class="spacetop-pages"></div>
+	<div class="container">
+	<h3 style="width: 935px;">Dairy Liquids</h3>
+		<div class="col-sm-12">
+			<div class="brands-tabs">
+				<div class="col-sm-6 details">
+					<div class="col-sm-12">
+						<div class="col-sm-6"><!-- Product Details 1 -->
+						<h4>Fresh Milk</h4>
+						<p>Available sizes<br>
+						1l<br>
+						180ml
+						</p>
+							
+						</div><!-- /Product Details 1 -->
+						<div class="col-sm-6"><!-- Product Image 1 -->
+							<div>
+								<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7days_TOFFEE-CROISSANT.png">
+							</div>
+						</div><!-- /Product Image 1 -->
+					</div>
+				</div>
+				<div class="col-sm-6 details">
+					<div class="col-sm-12">
+						<div class="col-sm-6"><!-- Product Details 2 -->
+						<h4>Fresh Yoghurt</h4>
+						<p>Available sizes<br>
+						2l<br>
+						950ml
+						</p>
+							
+						</div><!-- /Product Details 2 -->
+						<div class="col-sm-6"><!-- Product Image 2 -->
+							<div>
+								<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days_CHOCOLATE.png">
+							</div>
+						</div><!-- /Product Image 2 -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-12">
+			<div class="brands-tabs">
+				<div class="col-sm-6 details">
+					<div class="col-sm-12">
+						<div class="col-sm-6"><!-- Product Details 1 -->
+						<h4>Long Life Milk</h4>
+						<p>Available sizes<br>
+						1l<br>
+						</p>	
+						</div><!-- /Product Details 1 -->
+						<div class="col-sm-6"><!-- Product Image 1 -->
+							<div>
+								<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7days_TOFFEE-CROISSANT.png">
+							</div>
+						</div><!-- /Product Image 1 -->
+					</div>
+				</div>
+				<div class="col-sm-6 details">
+					<div class="col-sm-12">
+						<div class="col-sm-6 "><!-- Product Details 2 -->
+						<h4>Long Life Milk (Lactofree)</h4>
+						<p>Available sizes<br>
+						1l<br>
+						</p>
+							
+						</div><!-- /Product Details 2 -->
+						<div class="col-sm-6"><!-- Product Image 2 -->
+							<div>
+								<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days_CHOCOLATE.png">
+							</div>
+						</div><!-- /Product Image 2 -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-12">
+			<div class="brands-tabs">
+				<div class="col-sm-6 details">
+					<div class="col-sm-12">
+						<div class="col-sm-6"><!-- Product Details 1 -->
+						<h4>Flavored Long Life Milk (Maher the Adventurer)</h4>
+						<p>Available sizes<br>
+						125ml<br>
+						</p>	
+						</div><!-- /Product Details 1 -->
+						<div class="col-sm-6"><!-- Product Image 1 -->
+							<div>
+								<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days_CHOCOLATE.png">		
+							</div>
+						</div><!-- /Product Image 1 -->
+					</div>
+				</div>
+				<div class="col-sm-6 details">
+					<div class="col-sm-12">
+						<div class="col-sm-6 "><!-- Product Details 2 -->
+						<h4>Flavored Long Life Milk (NIjoom)</h4>
+						<p>Available sizes<br>
+						150ml<br>
+						</p>
+							
+						</div><!-- /Product Details 2 -->
+						<div class="col-sm-6"><!-- Product Image 2 -->
+							<div>
+								<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7days_TOFFEE-CROISSANT.png">
+							</div>
+						</div><!-- /Product Image 2 -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+		<!-- section end -->	
+
+<?php get_footer(); ?>
