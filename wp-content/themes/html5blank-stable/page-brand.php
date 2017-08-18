28c96893d60bfbbf7be1710b3bf9a67d1bdaf02d
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-18 09:40:25 +0200

    brand page added

---
+++ wp-content/themes/html5blank-stable/page-brand.php
@@ -0,0 +1,71 @@
+<?php /* Template Name: Template - Brand  */ ?>
+
+<?php get_header(); ?>
+		<!-- section start -->
+		<!-- ================ -->
+		<div class="spacetop-pages"></div>
+<div class="container">
+	<h2 id="about"><?php the_title(); ?></h2>
+	<div  class="col-sm-12">
+		<div style="margin-left: -15px;">
+		<h4>About 7DAYS</h4>
+			<div class="col-sm-9">
+				<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+				ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+				anim id est laborum."</p>
+
+			</div>
+			<div class="col-sm-3">
+				<div>
+					<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<h4>Products</h4>
+	<div class="col-sm-12">
+		<div class="brands-tabs">
+		<h3>Dairy Liquids</h3>
+			<div class="col-sm-9 brands-tabs-text">
+				<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+				dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+				ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+				anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">View Products</button></div></p>
+
+			</div>
+			<div class="col-sm-3 brands-tabs-image">
+				<div class="brands">
+					<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="col-sm-12">
+		<div class="brands-tabs">
+			<h3>Food</h3>
+				<div class="col-sm-9 brands-tabs-text">
+					<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+					dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+					ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+					anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">View Products</button></div></p>
+
+				</div>
+				<div class="col-sm-3 brands-tabs-image">
+					<div class="brands">
+						<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+		<!-- section end -->	
+
+<?php get_footer(); ?>
