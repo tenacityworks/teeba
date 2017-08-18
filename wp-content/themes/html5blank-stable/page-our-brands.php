be6ee5bb44b1c97a401a0c99ba598012eeb65927
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-08-18 15:19:45 +0200

    our brands php code

--- wp-content/themes/html5blank-stable/page-our-brands.php
+++ wp-content/themes/html5blank-stable/page-our-brands.php
@@ -1,105 +1,46 @@
-<?php get_header(); ?>
+<?php get_header(); 
+
+	$_args = array(
+	    'post_status'       => 'publish',
+	    'post_type'         => 'post',
+	    'offset'            => 0,
+	    'orderby'           => 'date',
+	    'posts_per_page'    => 8,
+	    'tax_query'         => array(
+	    array(
+	        'taxonomy'      => 'category',
+	        'field'         => 'slug',
+	        'terms'         => 'our-brands'
+	    )),
+	); 
+
+	$brands             = new WP_Query($_args);
+
+?>
 		<!-- section start -->
 		<!-- ================ -->
 		<div class="spacetop-pages"></div>
 		<div class="container">
-			<h2 id="about">Our Brands</h2>		
+			<h2 id="about">Our Brands</h2>	
+       <?php while ($brands->have_posts()) : $brands->the_post(); ?>	
 			<div  class="col-sm-12">
 				<div class="brands-tabs">
-				<h3>7DAYS</h3>
+				<h3><?php the_title(); ?></h3>
 					<div class="col-sm-9 brands-tabs-text">
-						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
-
+						<?php the_content(); ?>
+						<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div>
 					</div>
 					<div class="col-sm-3 brands-tabs-image">
 						<div class="brands">
-							<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
+							<?php the_post_thumbnail(); ?>
 						</div>
 					</div>
+					<!-- <div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div> -->
 				</div>
 			</div>
+		<?php endwhile; 
+              wp_reset_query(); ?>
 
-			<div class="col-sm-12">
-				<div class="brands-tabs">
-				<h3>Tropicana</h3>
-					<div class="col-sm-9 brands-tabs-text">
-						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
-
-					</div>
-					<div class="col-sm-3 brands-tabs-image">
-						<div class="brands">
-							<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
-						</div>
-					</div>
-				</div>
-			</div>
-
-
-		<div class="col-sm-12">
-			<div class="brands-tabs">
-				<h3>Teeba</h3>
-					<div class="col-sm-9 brands-tabs-text">
-						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
-
-					</div>
-					<div class="col-sm-3 brands-tabs-image">
-						<div class="brands">
-							<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
-						</div>
-					</div>
-				</div>
-			</div>
-
-
-		<div class="col-sm-12">
-			<div class="brands-tabs">
-				<h3>Almarai</h3>
-					<div class="col-sm-9 brands-tabs-text">
-						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
-
-					</div>
-					<div class="col-sm-3 brands-tabs-image">
-						<div class="brands">
-							<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
-						</div>
-					</div>
-				</div>
-			</div>
-
-		<div class="col-sm-12">
-			<div class="brands-tabs">
-				<h3>Pepsico</h3>
-					<div class="col-sm-9 brands-tabs-text">
-						<p style="padding: 15px;">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-						ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-						nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-						anim id est laborum."<div class="brands-more-dets"><button name="cv" type="submit">More Details</button></div></p>
-
-					</div>
-					<div class="col-sm-3 brands-tabs-image">
-						<div class="brands">
-							<img style="width: 100%; height: auto;" src="/wp-content/uploads/2017/08/7Days.png">
-						</div>
-					</div>
-				</div>
-			</div>
 	</div>
 		
 		<!-- section end -->	
