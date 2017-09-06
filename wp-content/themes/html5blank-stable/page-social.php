0a2db805ccf45a9baae763cd8079520d987f0d5a
Danai Nyandoro <dan@Danais-MacBook-Air.local>
2017-09-06 15:46:50 +0200

    social pages exits

--- wp-content/themes/html5blank-stable/page-social.php
+++ wp-content/themes/html5blank-stable/page-social.php
@@ -12,15 +12,11 @@
 				<div class="row">
 					<div class="col-sm-6 _socialleft">
 					<h3 class="h3social">Teeba on Facebook</h3><i class="fa fa-facebook-square fasocialpos"></i>
-						<?php echo $fb;
-
-						/* do_shortcode('[facebook-pagelike href="facebook" width="500" height="365" tabs="timeline, events, messages" small_header="false" align="left" hide_cover="false" show_facepile="false"]'); */
-
-						?>
+						<?php echo $fb;	?>
 					</div>
 					<div class="col-sm-6 _socialright">
 					<h3 class="h3social">Teeba on Instagram</h3><i class="fa fa-instagram fasocialpos"></i>
-						<iframe src="http://instaembedder.com/gallery-v2.php?width=440&cols=1&rows=5&margin=8&color=gray&likes=1&share_buttons=1&comments=1&date=1&link=1&frame=1&image_border=8&frame_color=white&cid=5677&" style="margin-top:34px;" scrolling='yes' frameborder="0" width="100%" height="365" ></iframe>
+						<iframe src="http://instaembedder.com/gallery-v2.php?width=430&cols=1&rows=5&margin=8&color=gray&likes=1&share_buttons=1&comments=1&date=1&link=1&frame=1&image_border=8&frame_color=white&cid=5677&" style="margin-top:34px;" scrolling='yes' frameborder="0" width="100%" height="365" ></iframe>
 					</div>
 				</div>
 			<!-- /Teeba Social -->
@@ -35,7 +31,7 @@
 					</div>
 				<div class="col-sm-6 _socialright">
 					<h3 class="h3social">Teeba on Instagram</h3><i class="fa fa-instagram fasocialpos"></i>
-						<iframe src="http://instaembedder.com/gallery-v2.php?width=440&cols=1&rows=5&margin=8&color=gray&likes=1&share_buttons=1&comments=1&date=1&link=1&frame=1&image_border=8&frame_color=white&cid=5677&" style="margin-top:34px;" scrolling='yes' frameborder="0" width="100%" height="365" ></iframe>
+						<iframe src="http://instaembedder.com/gallery-v2.php?width=430&cols=1&rows=5&margin=8&color=gray&likes=1&share_buttons=1&comments=1&date=1&link=1&frame=1&image_border=8&frame_color=white&cid=5677&" style="margin-top:34px;" scrolling='yes' frameborder="0" width="100%" height="365" ></iframe>
 					</div>
 				</div>
 			<!-- /Teeba Social -->
@@ -49,7 +45,7 @@
 					</div>
 				<div class="col-sm-6 _socialright">
 					<h3 class="h3social">Almarai on Instagram</h3><i class="fa fa-instagram fasocialpos"></i>
-						<iframe src="http://instaembedder.com/gallery-v2.php?width=440&cols=1&rows=5&margin=8&color=gray&likes=1&share_buttons=1&comments=1&date=1&link=1&frame=1&image_border=8&frame_color=white&cid=5677&" style="margin-top:34px;" scrolling='yes' frameborder="0" width="100%" height="365" ></iframe>
+						<iframe src="http://instaembedder.com/gallery-v2.php?width=430&cols=1&rows=5&margin=8&color=gray&likes=1&share_buttons=1&comments=1&date=1&link=1&frame=1&image_border=8&frame_color=white&cid=5677&" style="margin-top:34px;" scrolling='yes' frameborder="0" width="100%" height="365" ></iframe>
 					</div>
 				</div>
 				<!-- 	</div> -->
